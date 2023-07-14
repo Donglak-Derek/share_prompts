c5c800926aa4863d1547c5cb540d41356fd20d64
DerekHan <donglak.han@gmail.com>
2023-07-14 16:25:20 -0500

    Page and layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,22 @@
 const Home = () => {
-  return <div>Home</div>;
+  // fix flex-center?
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        발견하고 공유하자! <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          인공지능 Ai 프롬프트
+        </span>
+      </h1>
+      <p className="desc text-center">
+        프롬프트 천국은 오픈소스 인공지능 AI 시대에 필요한 프롬프팅 툴입니다.
+        <br className="max-md:hidden" />
+        프로프트를 창조하고 공유해요!
+      </p>
+
+      {/** Feed */}
+    </section>
+  );
 };
 
 export default Home;
