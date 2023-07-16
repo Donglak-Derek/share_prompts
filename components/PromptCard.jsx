03d918f83eee39483b03bfc828e0a3e4ce0b1035
DerekHan <donglak.han@gmail.com>
2023-07-16 12:11:29 -0500

    making tag clickable

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -42,6 +42,14 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
         </div>
       </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        #{post.tag}
+      </p>
     </div>
   );
 };
